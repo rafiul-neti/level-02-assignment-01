d06983a260b04d78a03871a830e8f0939e6d15aa
Rafiul Islam <irafiul210@gmail.com>
2026-06-27 08:26:33 +0600

    solved all the problems

--- solutions.ts
+++ solutions.ts
@@ -26,3 +26,44 @@ const getProperty = <X>(obj: X, key: keyof X) => {
   return obj[key];
 };
 
+// Solution 05
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+
+const toggleReadStatus = (obj: Book) => {
+  return { ...obj, isRead: true };
+};
+
+// Solution 06
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+// Solution 07
+const getIntersection = (input1: number[], input2: number[]): number[] => {
+  const set = new Set(input1);
+  const intersection = input2.filter((num) => set.has(num));
+  return intersection;
+};
