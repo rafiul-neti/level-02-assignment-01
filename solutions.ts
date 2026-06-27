dccd76bfc3b298e89b59600a02df94a1d7ba49b8
Rafiul Islam <irafiul210@gmail.com>
2026-06-27 07:00:14 +0600

    solved problem number 1 & 2

---
+++ solutions.ts
@@ -0,0 +1,14 @@
+// Solution 1
+const filterEvenNumbers = (arrOfNum: number[]): number[] => {
+  const evenNum = arrOfNum.filter((num) => num % 2 === 0);
+  return evenNum;
+};
+
+// Solution 2
+const reverseString = (str: string): string => {
+  const reversed = str.split("").reverse().join("");
+  return reversed;
+};
+
+// Solution 03
+
